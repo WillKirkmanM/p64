1576520e7c79f8374a4e2b4f5453fc740686ae06
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-12-12 21:34:09 +0000

    Feat(config.rs): Input Configuration

---
+++ crates/p64-gui/src/config.rs
@@ -0,0 +1,41 @@
+use crate::input;
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct InputProfile {
+    pub keys: [(bool, i32); 18],
+    pub controller_buttons: [(bool, i32); 14],
+    pub controller_axis: [(bool, i32, i16); 18],
+    pub joystick_buttons: [(bool, u32); 14],
+    pub joystick_hat: [(bool, u32, i8); 14],
+    pub joystick_axis: [(bool, u32, i16); 18],
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+pub struct Input {
+    pub input_profiles: std::collections::HashMap<String, InputProfile>,
+    pub input_profile_binding: [String; 4],
+    pub controller_assignment: [Option<String>; 4],
+}
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+pub struct Config {
+    pub input: Input,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        let mut input_profiles = std::collections::HashMap::new();
+        input_profiles.insert("Default".to_string(), input::get_default_profile());
+        Config {
+            input: Input {
+                input_profile_binding: [
+                    "Default".to_string(),
+                    "Default".to_string(),
+                    "Default".to_string(),
+                    "Default".to_string(),
+                ],
+                controller_assignment: [None, None, None, None],
+                input_profiles,
+            },
+        }
+    }
+}
